a792472af9a496eeceb7a3c5c18a45e50533330a
John Chan <chenfazhun@163.com>
2017-10-11 09:35:20 +0800

    buchong

--- update.sql
+++ update.sql
@@ -2,6 +2,12 @@ ALTER TABLE `xunjian`.`xj_templet_point`
 ADD COLUMN `order_by` INT NULL DEFAULT 0 AFTER `point_id`;
 
 ALTER TABLE `xunjian`.`xj_templet_point`
+DROP FOREIGN KEY `xj_templet_point_ibfk_2`,
+DROP FOREIGN KEY `xj_templet_point_ibfk_1`;
+ALTER TABLE `xunjian`.`xj_templet_point`
+DROP INDEX `point_id` ;
+
+ALTER TABLE `xunjian`.`xj_templet_point`
 ADD COLUMN `id` INT NOT NULL AUTO_INCREMENT FIRST,
 DROP PRIMARY KEY,
 ADD PRIMARY KEY (`id`, `templet_id`, `point_id`);
